f0c03c5ecaca856f8ab88ccc17095a7a6540ac43
scottmeng <blackridermkz@gmail.com>
2013-11-17 23:18:57 +0800

    additional input check function

--- coherence.cpp
+++ coherence.cpp
@@ -88,31 +88,75 @@ class cache {
 		}
 };
 
+/*
+ * check if user inputs are still valid
+ * prompt users of the correct input format
+ */
+bool areInputsValid(char * usrProtocol, char * usrInputFile, int usrNoProcessors,
+					int usrBlockSize, int usrCacheSize, int usrAssociativity) {
+	if(!strncmp(usrInputFile, FTT_FILE, sizeof(FTT_FILE)) && 
+	   !strncmp(usrInputFile, WEATHER_FILE, sizeof(WEATHER_FILE))) {
+		printf("Input file must be either'WEATHER' or 'FTT'. Please try again.\n");
+		return false;
+	}
+
+	if(!strncmp(usrProtocol, DRAGON_PROTOCOL, sizeof(DRAGON_PROTOCOL)) && 
+	   !strncmp(usrProtocol, MESI_PROTOCOL, sizeof(MESI_PROTOCOL))) {
+		printf("Protocol must be either 'DRAGON' or 'MESI'. Please try again.\n");
+		return false;
+	}
+
+	if((usrNoProcessors != 1) && 
+	   (usrNoProcessors != 2) &&
+	   (usrNoProcessors != 3) &&
+	   (usrNoProcessors != 4)) {
+		printf("Number of processors must be 1, 2, 4 or 8. Please try again.\n");
+		return false;
+	}
+
+	if((usrBlockSize % 16) != 0) {
+		printf("Block size must be a multiple of 16(default word size). Please try again.\n");
+		return false;
+	}
+
+	if((usrCacheSize % (usrBlockSize * usrAssociativity)) != 0) {
+		printf("Cache size must be a multiple of the production of block size and associativity. Please try again.\n");
+		return false;
+	}
+
+	return true;
+} 
+
 int main(int argc, char * argv[]) {	
 
 	// user inputs
 	int noProcessors, blockSize, cacheSize, associativity;
-	char * usrProtocol, * inputFile;
+	char * protocol, * inputFile;
 	bool isDragon = false, isWeather = false;
 
 	// performance statistics
 
 	// read user inputs
 	// cache-size, isAssociative, #processors, block-size, input-file
-
 	if(argc != 7) {
 		printf("Please follow the format specified: 'coherence protocol input_file no_processors cache_size associativity block_size'.\n");
 		exit(EXIT_FAILURE);
 	}
 
-	usrProtocol = argv[1];
+	protocol = argv[1];
 	inputFile = argv[2];
 	noProcessors = atoi(argv[3]);
 	cacheSize = atoi(argv[4]);
 	associativity = atoi(argv[5]);
 	blockSize = atoi(argv[6]);
 
-	if(strncmp(usrProtocol, DRAGON_PROTOCOL, sizeof(DRAGON_PROTOCOL))) {
+	// check if inputs are valid
+	if(!areInputsValid(protocol, inputFile, noProcessors, 
+					   cacheSize, associativity, blockSize)) {
+		exit(EXIT_FAILURE);
+	}
+
+	if(strncmp(protocol, DRAGON_PROTOCOL, sizeof(DRAGON_PROTOCOL))) {
 		isDragon = true;
 	}
 
@@ -131,7 +175,7 @@ int main(int argc, char * argv[]) {
 		strcat(indiFileName, ".prg");
 
 		printf(indiFileName);
-		//files[i] = fopen(indiFileName, "r");
+		files[i] = fopen(indiFileName, "r");
 	}
 
 	exit(0);		// for testing purpose
@@ -160,6 +204,12 @@ int main(int argc, char * argv[]) {
 	}
 
 	// output statistics
+
+
+	// close files
+	for(int i=1; i<=noProcessors; i++) {
+		fclose(files[i]);
+	}
 
 	return 1;
 }
